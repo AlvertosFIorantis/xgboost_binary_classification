9f4770aca3dd78ffc667d6fec3941bdcad180dd5
Alvertos <fiorantis-al@hotmail.com>
2021-12-12 18:48:12 +0000

    add conversion from log odds to probabilities

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#%%
 from pandas import DataFrame
 import datetime
 import pandas as pd
@@ -14,7 +15,7 @@ from sklearn.metrics import precision_score
 from sklearn.metrics import accuracy_score
 
 
-
+#%%
 df_2 = pd.read_csv("./titanic.csv", sep="\t")
 
 
@@ -152,14 +153,14 @@ Y_test = test[test.columns[target_idx]]
 
 
 X_train, cat_columns, cat_dummies, processed_columns = one_hot_encoding_train(
-    X_train, True)
+    X_train, False)
 
-X_validation = one_hot_encoding_test(X_validation, True)
+X_validation = one_hot_encoding_test(X_validation, False)
 
-X_test = one_hot_encoding_test(X_test, True)
+X_test = one_hot_encoding_test(X_test, False)
 
 
-
+#%%
 '''Baysian optimization'''
 
 from datetime import datetime
@@ -174,23 +175,34 @@ def timer(start_time=None):
 
 def xgb_evaluate(min_child_weight, gamma, subsample, colsample_bytree, max_depth, learning_rate, scale_pos_weight, num_round,reg_lambda,reg_alpha): #max_depth
     max_depth = int(max_depth)
-    best = xgb.XGBClassifier(min_child_weight = min_child_weight, gamma = gamma, subsample = subsample, colsample_bytree = colsample_bytree, 
-    max_depth = int(max_depth), learning_rate = learning_rate, num_round = num_round, scale_pos_weight = scale_pos_weight,reg_lambda=reg_lambda,reg_alpha=reg_alpha, seed=16)
-    best.fit(X_train, Y_train)
+    num_round=int(num_round)
+    params = {
+        "objective":"binary:logistic"
+        ,"eval_metric":"error"
+        ,"colsample_bytree":colsample_bytree
+        ,"min_child_weight":min_child_weight
+        ,"subsample":subsample
+        ,"eta":learning_rate
+        ,"max_depth":max_depth
+        ,"gamma":gamma
+        ,"scale_pos_weight":scale_pos_weight
+        ,"reg_lambda":reg_lambda
+        ,"reg_alpha":reg_alpha
+    }
 
-    valid_y_pred = best.predict(X_validation)
-    cm = confusion_matrix(Y_validation, valid_y_pred)
-    #presision = cm[1][1]/(cm[1][1]+cm[1][0])
-    #Accuracy
-    #presision=(cm[0][0]+cm[1][1])/(cm[0][0]+cm[1][1]+cm[1][0]+cm[0][1])
-    #presision=(cm[1][1])/(cm[1][1]+cm[0][1])
-    #presision=((cm[1][1]/cm[0][1]))+0.000000001#otherwise i can get in inf value in which case the optimizer will crush
-    #presision=cm[1][1]
-    #presision=(recall_score(Y_validation, valid_y_pred)+precision_score(Y_validation, valid_y_pred))
-    #presision=precision_score(Y_validation, valid_y_pred)
-    presision = accuracy_score(Y_validation,valid_y_pred)
-    #presision=recall_score(Y_validation, valid_y_pred)
-    return presision
+    bst =xgb.train(params,data_dmatrix,num_round)
+
+    valid_y_pred = bst.predict(validation_matrix)
+    threshold = np.percentile(valid_y_pred,np.arange(0,100,10))[-1]
+
+    predictions=[]
+    for value in valid_y_pred:
+        if value>=threshold:
+            predictions.append(1.0)
+        else:
+            predictions.append(0.0)
+    recall = recall_score(Y_validation,predictions)
+    return recall
 
 
 from bayes_opt import BayesianOptimization
@@ -225,13 +237,13 @@ data_dmatrix = xgb.DMatrix(data=X_train, label=Y_train)
 validation_matrix = xgb.DMatrix(data=X_validation, label=Y_validation)
 watchlist = [(validation_matrix, 'eval'), (data_dmatrix, 'train')]
 
-
+#%%
 bst = xgb.train(params, data_dmatrix,num_round,
                 watchlist)
 
 
 from xgboost import plot_importance
-plot_importance(bst, max_num_features=10,importance_type='weight')
+#plot_importance(bst, max_num_features=10,importance_type='weight')
 
 '''Create confusion matrix for churn model '''
 valid_y_pred = bst.predict(validation_matrix)
@@ -245,7 +257,48 @@ print(precision_score(Y_validation, valid_y_pred))
 print(accuracy_score(Y_validation,valid_y_pred))
 print(confusion_matrix(Y_validation, valid_y_pred))
 
+#%%
 
+'''
+Plot recall per decile
+'''
+
+test_y_pred = bst.predict(validation_matrix)
+
+deciles = []
+recalls = []
+precisions = []
+thresholds = []
+for n in range(1,11):
+    deciles.append(n)
+
+    threshold = np.percentile(test_y_pred,np.arange(0,100,10))[-n]
+    thresholds.append(threshold)
+
+    predictions = []
+    for value in test_y_pred:
+        if value >= threshold:
+            predictions.append(1.0)
+        else:
+            predictions.append(0.0)
+    
+    recall = recall_score(Y_validation,predictions)
+    recalls.append(recall)
+
+    precission = precision_score(Y_validation,predictions)
+    predictions.append(precission)
+
+fig,ax = plt.subplots(figsize=(10,5))
+plt.plot(deciles,[round(x*100,1) for x in recalls],marker='o',c='Indigo',markersize=15,linewidth=3)
+plot.xlabel('Decile',fontsize=15)
+plot.ylabel('Fraction of total conversions recovered [%]',fontsize=15)
+plt.xticks(np.arange(1.10+1,1),fontsize=15)
+plt.yticks(np.arange(10,100+1,10),fontsize=15)
+plt.grid()
+plt.show()
+
+
+#%%
 import shap
 explainer  = shap.Explainer(bst,X_validation)
 shap_values = explainer(X_validation)
@@ -284,16 +337,14 @@ def obs_to_explain(data,base_value ,shap_values,n):
 
         
         return(for_plot ) 
-    
-#n = number of most important features
-most_important_features= obs_to_explain(X_validation.iloc[obs,:],
-                              base_value = base_value,
-                              shap_values = rf_shap_values[obs],n=8)
+
+
+#%%
 
 
 
 
-
+#%%
 #Check that the values matches the values in the plot
 obs=10
 shap.plots.waterfall(shap_values[obs])
@@ -304,3 +355,252 @@ print(most_important_features)
 print(X_validation.iloc[obs,:])
 
 
+
+# %%
+''''
+Converting log odds to probabilties
+'''
+explainerModel_prob = shap.TreeExplainer(bst,data = X_train)
+
+shap_values_model_prob  = explainerModel_prob.shap_values(X_train)
+
+ 
+
+def xgb_shap_transform_scale(shap_values, model_prediction):
+
+   
+
+    #Compute the transformed base value, which consists in applying the logit function to the base value
+
+    from scipy.special import expit #Importing the logit function for the base value transformation
+
+    untransformed_base_value = shap_values[-1]
+
+    base_value = expit(untransformed_base_value )
+
+   
+
+    #Computing the original_explanation_distance to construct the distance_coefficient later on
+
+    original_explanation_distance = sum(shap_values)
+
+ 
+
+    #Computing the distance between the model_prediction and the transformed base_value
+
+    distance_to_explain = (model_prediction - base_value)
+
+ 
+
+    #The distance_coefficient is the ratio between both distances which will be used later on
+
+    distance_coefficient = original_explanation_distance / distance_to_explain
+
+ 
+
+    #Transforming the original shapley values to the new scale
+
+    shap_values_transformed = shap_values / distance_coefficient
+
+ 
+
+    #Finally resetting the base_value as it does not need to be transformed
+
+    shap_values_transformed [-1] = base_value
+
+   
+
+    #Now returning the transformed array
+
+    return shap_values_transformed
+
+ 
+
+ 
+
+j=0
+
+#the 10th observation of the dataset
+
+shap_values=shap_values_model_prob[j]
+
+model_prediction=bst.predict(data_dmatrix)[j]
+
+shap_proba=xgb_shap_transform_scale(shap_values, model_prediction)
+
+print("Probabiltiy from the model:#...",model_prediction)
+
+print("sum of all the probabities that we get for each feature fomr the shap package..",sum(shap_proba))
+
+ 
+
+ 
+
+ 
+
+#Get the position in array of the 10 larger values
+
+arr =shap_values
+
+ 
+
+ 
+
+large_indexes=arr.argsort()[-10:][::-1] #get the position of the 10 largest values
+
+ 
+
+largest_values=np.sort(arr)[-10:][::-1]# 10 largest values
+
+ 
+
+smallest_values=np.sort(arr)[:10] # 10 lowest values
+
+ 
+
+smallest_indexes=arr.argsort()[:10] #indexes of the 10 lowest vlues !!!!!!!!! it was 9 !!!!!!!!!!!!
+
+ 
+
+ 
+
+#create an array for teh biggest values where the key is the index and the value is teh value of the array
+
+ 
+
+large_values= {}
+
+for A, B in zip(large_indexes, largest_values):
+
+    large_values[A] = B
+
+ 
+
+small_values= {}
+
+for A, B in zip(smallest_indexes, smallest_values):
+
+    small_values[A] = B
+
+ 
+
+#merge the 2 dictionaries
+
+combined_dictionary = {**large_values, **small_values}
+
+ 
+
+combined_dictionary
+
+ 
+
+main_keys = sorted(combined_dictionary, key=lambda dict_key: abs(combined_dictionary[dict_key]))[-10:]
+
+#the 10 keys with the largest absolute values
+
+ 
+
+final_dictioanry = dict((k, combined_dictionary[k]) for k in main_keys if k in combined_dictionary)
+
+ 
+
+ 
+
+ 
+
+for key, value in final_dictioanry.items():
+
+    colname = X_train.columns[key]
+
+    print(f"Column name {colname} the value is {value}")
+
+ 
+
+ 
+
+#check that my numbers matches shap waterfall graph
+
+explainer_test = shap.Explainer(bst)
+
+shap_values_test = explainer_test(X_train)
+
+shap.plots.waterfall(shap_values_test[0],max_display=20)
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+column_names = ['Explanation_Variable_1','Explanation_Variable_1_value','Explanation_Variable_2'
+
+                          ,'Explanation_Variable_2_value',"Explanation_Variable_3",
+
+                          'Explanation_Variable_3_value','Explanation_Variable_4',"Explanation_Variable_4_value",
+
+                          'Explanation_Variable_5',
+
+                          'Explanation_Variable_5_value','Explanation_Variable_6','Explanation_Variable_6_value',
+
+                          "Explanation_Variable_7",'Explanation_Variable_7_value','Explanation_Variable_8',
+
+                          "Explanation_Variable_8_value", "Explanation_Variable_9",'Explanation_Variable_9_value'
+
+                          ,'Explanation_Variable_10','Explanation_Variable_10_value']
+
+final_dataframe = pd.DataFrame(columns=column_names)
+
+ 
+
+ 
+
+ 
+
+row_list = []
+
+for key, value in final_dictioanry.items():
+
+    colname = X_train.columns[key]
+
+    # i am using insert instead of append so i can add the value in the begining of the list
+
+    row_list.insert(0, value)
+
+    row_list.insert(0, colname)
+
+   
+
+row_df = pd.DataFrame([row_list],columns=column_names,dtype=str) # i foce all columns to be strings helps with the json
+
+final_dataframe = final_dataframe.append(row_df, ignore_index=True)
+
+ 
+
+ 
+
+final_dataframe.head()
+
+ 
+
+ 
+
+#Try to convert the row to json instead of creating new columns
+
+row_df.astype(str)
+
+row_json_similar_to_DSS = row_df.to_json(orient='records')[1:-1].replace('},{', '} {')
+
+ 
+
+ 
+
+ 
+
+row_json_similar_to_DSS
+# %%
